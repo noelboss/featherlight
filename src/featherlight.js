f5f31ece048a83c2b30e8c690fa42d0aba13885c
Noël Bossart <noel.bossart@namics.com>
2013-10-24 10:10:44 +0200

    Added standalone function to allow for ajax calls

--- src/featherlight.js
+++ src/featherlight.js
@@ -8,51 +8,70 @@
 (function($) {
 	"use strict";
 
+	var setupFeatherlight = function(c, $content){
+		var t = this,
+			$t = $(this),
+			variant = $t.attr('data-'+c.namespace+'-variant'),
+			$bg = $(c.bg || '<div class="'+c.namespace+'"><div class="'+c.namespace+'-content"><span class="'+c.namespace+'-close">X</span></div></div>');
+
+		if($content.length < 1){
+			t.$content = $($t.attr(c.targetAttr), c.context);
+		} else {
+			t.$content = $content;
+		}
+
+		if(t.$content.length > 0){
+			// clone DOM for the background, wrapper and the closebutton
+			t.$fl = $bg.clone();
+
+			// add css class to override styles
+			if(variant){
+				t.$fl.addClass(variant);
+			}
+
+			// bind close function on close button
+			t.$fl.find('.'+c.namespace+'-close')
+				.on(c.closeTrigger+'.'+c.namespace, $.proxy(c.close, t))
+				.after(t.$content.show()); // add content after closebutton
+
+			// close when click on background
+			if(c.clickBgClose){
+				t.$fl.on(c.closeTrigger+'.'+c.namespace, $.proxy(c.close, t));
+			}
+
+			// bind open function
+			if(t.hasOwnProperty('nodeName')){
+				$t.on(c.openTrigger+'.'+c.namespace, $.proxy(c.open, t));
+			} else {
+				$.proxy(c.open, t)();
+			}
+		}
+	};
+
+	var fl = {
+		featherlight: function($content, config) {
+			var c = $.extend({}, $.fn.featherlight.defaults, config),
+				$content = $content || $();
+
+			if(typeof $content === 'string'){
+				$content = $($content);
+			}
+			if($content.length > 0){
+				$.proxy(setupFeatherlight, {}, c, $content)();
+			}
+		}
+	};
+
+	$.extend($, fl);
+
 	// extend jQuery
 	$.fn.featherlight = function(config, $content) {
-		var $a = $(this),                // contains the triggering element
-			$bg = null,                  // contains background
-			$content = $content || $(),  // contains content (lightbox)
-			$fl = null,                  // contains content (lightbox) and background
-			c = config || {};            // user config
-
-		c =  $.extend({}, $.fn.featherlight.defaults, c);
-		$bg = $(c.bg || '<div class="'+c.namespace+'"><div class="'+c.namespace+'-content"><span class="'+c.namespace+'-close">X</span></div></div>');
-
-		$a.each(function(){
-			var t = this,
-				$t = $(this),
-				variant = $t.attr('data-'+c.namespace+'-variant');
-
-			if($content.length < 1){
-				t.$content = $($t.attr(c.targetAttr), c.context);
-			} else {
-				t.$content = $content;
-			}
-			if(t.$content.length > 0){
-				// clone DOM for the background, wrapper and the closebutton
-				t.$fl = $bg.clone();
-
-				// add css class to override styles
-				if(variant){
-					t.$fl.addClass(variant);
-				}
-
-				// bind close function on close button
-				t.$fl.find('.'+c.namespace+'-close')
-					.on(c.closeTrigger+'.'+c.namespace, $.proxy(c.close, t))
-					.after(t.$content.show()); // add content after closebutton
-
-				// close when click on background
-				if(c.clickBgClose){
-					t.$fl.on(c.closeTrigger+'.'+c.namespace, $.proxy(c.close, t));
-				}
-
-				// bind open function
-				$t.on(c.openTrigger+'.'+c.namespace, $.proxy(c.open, t));
-			}
+		var c = $.extend({}, $.fn.featherlight.defaults, config),
+				$content = $content || $();
+		$(this).each(function(){
+			$.proxy(setupFeatherlight, this, c, $content)();
 		});
-	};
+	}
 
 	// featherlight defaults
 	$.fn.featherlight.defaults = {
@@ -65,17 +84,17 @@
 		clickBgClose: true,                   // close lightbox on click on the background
 		bg: null,                             // custom DOM for the background, wrapper and the closebutton
 		autostart:    true,                   // initialize all links with that match "selector" on document ready
-		open: function(e){                     // opens the lightbox "this" contains $fl with the lightbox, and c with the configuration
+		open: function(e){                    // opens the lightbox "this" contains $fl with the lightbox, and c with the configuration
 			var t = this;
 			t.$fl.prependTo('body').fadeIn();
-			e.preventDefault();
+			if(e) e.preventDefault();
 		},
 		close: function(e){                    // closes the lightbox "this" contains $fl with the lightbox, and c with the configuration
 			var t = this;
 			t.$fl.fadeOut(function(){
 				t.$fl.detach();
 			});
-			e.preventDefault();
+			if (e) e.preventDefault();
 		}
 	};
 
