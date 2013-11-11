2ac91b09c050b01c5921c4fce69735ba166b71a4
Noël Bossart <noel.bossart@namics.com>
2013-11-11 23:04:51 +0100

    Improved code readability

--- src/featherlight.js
+++ src/featherlight.js
@@ -10,70 +10,70 @@
 
 	/* featherlight object */
 	var fl = {
-		defaults: { /* you can access and override all defaults using $.featherlight.defaults */
+		defaults: { /* you can access and override all defaults using $.fl.defaults */
 			selector:     '[data-featherlight]',  /* elements that trigger the lightbox */
 			context:      'body',                 /* context used to search for the lightbox content and triggers */
-			targetAttr:   'data-featherlight',    /* attribut of the triggered element that contains the selector to the lightbox content */
+			targetAttr:   'data-featherlight',    /* attribute of the triggered element that contains the selector to the lightbox content */
 			openTrigger:  'click',                /* event that triggers the lightbox */
 			closeTrigger: 'click',                /* event that triggers the closing of the lightbox */
 			namespace:    'featherlight',         /* name of the events and css class prefix */
 			resetCss:     false,                  /* reset all css */
-			variant:      null,                   /* class that will be added to change look of the lightbox instance */
+			variant:      null,                   /* class that will be added to change look of the lightbox */
 			clickBgClose: true,                   /* close lightbox on click on the background */
-			bg: null,                             /* custom DOM for the background, wrapper and the closebutton */
+			background:   null,                   /* custom DOM for the background, wrapper and the closebutton */
 			autostart:    true,                   /* initialize all links with that match "selector" on document ready */
-			open: function(e){                    /* opens the lightbox "this" contains $fl with the lightbox, and c with the configuration */
-				$.proxy($.featherlight.methods.open, this, e)();
+			open: function(event){                /* opens the lightbox "this" contains $instance with the lightbox, and with the config */
+				$.proxy($.featherlight.methods.open, this, event)();
 			},
-			close: function(e){                   /* closes the lightbox "this" contains $fl with the lightbox, and c with the configuration */
-				$.proxy($.featherlight.methods.close, this, e)();
+			close: function(event){                   /* closes the lightbox "this" contains $instance with the lightbox, and with the config */
+				$.proxy($.featherlight.methods.close, this, event)();
 			}
 		},
-		methods: { /* you can access and override all methods using $.featherlight.methods */
+		methods: { /* you can access and override all methods using $.fl.methods */
 			setup: function(config, content){
 				var $elm = $(this) || $(),
-					c = $.extend({}, fl.defaults, config),
-					variant = $elm.attr('data-'+c.namespace+'-variant') || c.variant,
-					css = !c.resetCss ? c.namespace : c.namespace+'-reset', /* by adding -reset to the classname, we reset all the default css */
-					$bg = $(c.bg || '<div class="'+css+'"><div class="'+css+'-content"><span class="'+css+'-close">X</span></div></div>'),
+					config = $.extend({}, fl.defaults, config),
+					variant = $elm.attr('data-'+config.namespace+'-variant') || config.variant,
+					css = !config.resetCss ? config.namespace : config.namespace+'-reset', /* by adding -reset to the classname, we reset all the default css */
+					$background = $(config.background || '<div class="'+css+'"><div class="'+css+'-content"><span class="'+css+'-close">X</span></div></div>'),
 
-					/* everything that we need later is stored in t (target) */
-					t = {
-						c: c,
+					/* everything that we need later is stored in self (target) */
+					self = {
+						config: config,
 						content: content,
 						$elm: $elm,
-						$fl: $bg.clone().addClass(variant) /* clone DOM for the background, wrapper and the closebutton */
+						$instance: $background.clone().addClass(variant) /* clone DOM for the background, wrapper and the closebutton */
 					};
 
 				/* close when click on background */
-				t.$fl.on(c.closeTrigger+'.'+c.namespace, $.proxy(c.close, t));
+				self.$instance.on(config.closeTrigger+'.'+config.namespace, $.proxy(config.close, self));
 
 				/* bind or call open function */
 				if($elm.length > 0 && this.tagName){
-					$elm.on(c.openTrigger+'.'+c.namespace, $.proxy(c.open, t));
+					$elm.on(config.openTrigger+'.'+config.namespace, $.proxy(config.open, self));
 				} else {
-					$.proxy(c.open, t)();
+					$.proxy(config.open, self)();
 				}
 			},
 
 			/* this method prepares the content and converts it into a jQuery object */
 			getContent: function(){
-				var t = this,
-					content = t.content,
-					attr = t.$elm.attr(t.c.targetAttr);
+				var self = this,
+					content = self.content,
+					attr = self.$elm.attr(self.config.targetAttr);
 
 				/* if we have DOM, convert to jQuery Object */
 				if(typeof content === 'string'){
-					t.content = $(content);
+					self.content = $(content);
 				} else if(content instanceof $ === false){ /* if we have no jQuery Object */
 					/* check if we have an image and create element */
 					if(attr === 'image' || attr.match(/\.(png|jpg|jpeg|gif|tiff|bmp)$/i)){
-						var url = attr.match(/\.(png|jpg|jpeg|gif|tiff|bmp)$/i) ? attr : t.$elm.attr('href');
-						t.content = $('<img src="'+url+'" alt="" class="'+t.c.namespace+'-image" />');
+						var url = attr.match(/\.(png|jpg|jpeg|gif|tiff|bmp)$/i) ? attr : self.$elm.attr('href');
+						self.content = $('<img src="'+url+'" alt="" class="'+self.config.namespace+'-image" />');
 					}
 					/* check if we have an ajax link */
 					else if(attr === 'ajax' || attr.match(/(http|htm|php)/i)){
-						var url = attr.match(/(http|htm|php)/i) ? attr : t.$elm.attr('href'),
+						var url = attr.match(/(http|htm|php)/i) ? attr : self.$elm.attr('href'),
 						/* we are using load so one can specify a target with: url.html #targetelement */
 						content = url ? $('<div></div>').load(url, function(response, status){
 							if ( status !== "error" ) {
@@ -82,43 +82,43 @@
 						}) : null;
 						return false;
 					}
-					/* otherwise create jquery elemnt by using the attribute as selector */
+					/* otherwise create jquery element by using the attribute as selector */
 					else {
-						t.content = $($(attr), t.c.context);
+						self.content = $($(attr), self.config.context);
 					}
 				}
 
 				/* we need a special class for the iframe */
-				if(t.content.is('iframe') || $('iframe', t.content).length > 0){
-					t.$fl.addClass(t.c.namespace+'-iframe');
+				if(self.content.is('iframe') || $('iframe', self.content).length > 0){
+					self.$instance.addClass(self.config.namespace+'-iframe');
 				}
-				t.content.addClass(t.c.namespace+'-inner');
+				self.content.addClass(self.config.namespace+'-inner');
 			},
 
-			/* opens the lightbox "this" contains $fl with the lightbox, and c with the configuration */
-			open: function(e){
-				if(e){e.preventDefault();}
-				var t = this;
+			/* opens the lightbox "this" contains $instance with the lightbox, and with the config */
+			open: function(event){
+				if(event){event.preventDefault();}
+				var self = this;
 
 				/* If we have content, add it and show lightbox */
-				if($.proxy(fl.methods.getContent, t)() !== false){
-					t.$fl
+				if($.proxy(fl.methods.getContent, self)() !== false){
+					self.$instance
 						.prependTo('body').fadeIn()
-						.find('.'+t.c.namespace+'-close')
-						.after(t.content);
+						.find('.'+self.config.namespace+'-close')
+						.after(self.content);
 				}
 			},
 
-			/* closes the lightbox "this" contains $fl with the lightbox, and c with the configuration */
-			close: function(e){
-				if(e){e.preventDefault();}
-				var t = this,
-					c = t.c,
-					$t = $(e.target);
+			/* closes the lightbox "this" contains $instance with the lightbox, and with the config */
+			close: function(event){
+				if(event){event.preventDefault();}
+				var self = this,
+					config = self.config,
+					$instance = $(event.target);
 
-				if((c.clickBgClose && $t.is('.'+c.namespace)) || $t.is('.'+c.namespace+'-close') ){
-					t.$fl.fadeOut(function(){
-						t.$fl.detach();
+				if((config.clickBgClose && $instance.is('.'+config.namespace)) || $instance.is('.'+config.namespace+'-close') ){
+					self.$instance.fadeOut(function(){
+						self.$instance.detach();
 					});
 				}
 			}
@@ -142,9 +142,9 @@
 
 	/* bind featherlight on ready if config autostart is true */
 	$(document).ready(function(){
-		var c = $.featherlight.defaults;
-		if(c.autostart){
-			$(c.selector, c.context).featherlight();
+		var config = $.featherlight.defaults;
+		if(config.autostart){
+			$(config.selector, config.context).featherlight();
 		}
 	});
 }(jQuery));
