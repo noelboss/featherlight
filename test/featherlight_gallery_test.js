ef987b71b7cfc3de7fa9bce4639b0fbe8d484060
Marc-Andre Lafortune <github@marc-andre.ca>
2014-04-01 15:47:07 -0400

    + basic gallery test

---
+++ test/featherlight_gallery_test.js
@@ -0,0 +1,30 @@
+if(!this.chai) { chai = require("chai"); }
+var expect = chai.expect;
+
+(function($) {
+  var $htmlFixtures = null;
+  var resetFixtures = function(){
+    if (!$htmlFixtures) $htmlFixtures = $('#fixtures').detach();
+    $('body >:not(#mocha)').remove()
+    $('body').append($htmlFixtures.clone());
+  };
+  $.fx.off = true;
+
+  describe('Featherlight Gallery', function() {
+    beforeEach(resetFixtures);
+    after(resetFixtures);
+
+    it ('works as expected', function() {
+      $('#basic-test a').featherlightGallery();
+      $('#basic-test a').eq(2).click();
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?2');
+      $('.featherlight').trigger('next');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?3');
+      $('.featherlight').trigger('next');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?0');
+      $('.featherlight').trigger('previous');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?3');
+    });
+
+  });
+}(jQuery));
