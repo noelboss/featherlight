a899e1c59afd66e501ba385b6120c1c8356477e9
Marc-Andre Lafortune <github@marc-andre.ca>
2014-04-16 13:58:18 -0400

    Fix fixture path in tests [ci skip]

--- test/featherlight_gallery_test.js
+++ test/featherlight_gallery_test.js
@@ -17,13 +17,13 @@ var expect = chai.expect;
     it ('works as expected', function() {
       $('#basic-test a').featherlightGallery();
       $('#basic-test a').eq(2).click();
-      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?2');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpg?2');
       $('.featherlight').trigger('next');
-      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?3');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpg?3');
       $('.featherlight').trigger('next');
-      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?0');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpg?0');
       $('.featherlight').trigger('previous');
-      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpeg?3');
+      expect($('.featherlight img')).to.have.attr('src').equal('fixtures/photo_large.jpg?3');
     });
 
   });
