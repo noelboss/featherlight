05a8eaab85acd85676ce90965a9732e6d21a0345
Marc-Andre Lafortune <github@marc-andre.ca>
2014-05-29 20:10:35 +0200

    Tweak alignment

--- src/featherlight.js
+++ src/featherlight.js
@@ -56,177 +56,177 @@
 		};
 
 	Featherlight.prototype = {
-			constructor: Featherlight,
-			/*** defaults ***/
-			/* extend featherlight with defaults and methods */
-			namespace:    'featherlight',         /* Name of the events and css class prefix */
-			targetAttr:   'data-featherlight',    /* Attribute of the triggered element that contains the selector to the lightbox content */
-			variant:      null,                   /* Class that will be added to change look of the lightbox */
-			resetCss:     false,                  /* Reset all css */
-			background:   null,                   /* Custom DOM for the background, wrapper and the closebutton */
-			openTrigger:  'click',                /* Event that triggers the lightbox */
-			closeTrigger: 'click',                /* Event that triggers the closing of the lightbox */
-			filter:       null,                   /* Selector to filter events. Think $(...).on('click', filter, eventHandler) */
-			root:         'body',                 /* Where to append featherlights */
-			openSpeed:    250,                    /* Duration of opening animation */
-			closeSpeed:   250,                    /* Duration of closing animation */
-			closeOnClick: 'background',           /* Close lightbox on click ('background', 'anywhere' or false) */
-			closeOnEsc:   true,                   /* Close lightbox when pressing esc */
-			closeIcon:    '&#10005;',             /* Close icon */
-			otherClose:   null,                   /* Selector for alternate close buttons (e.g. "a.close") */
-			beforeOpen:   $.noop,                 /* Called before open. can return false to prevent opening of lightbox. Gets event as parameter, this contains all data */
-			beforeClose:  $.noop,                 /* Called before close. can return false to prevent opening of lightbox. Gets event as parameter, this contains all data */
-			afterOpen:    $.noop,                 /* Called after open. Gets event as parameter, this contains all data */
-			afterClose:   $.noop,                 /* Called after close. Gets event as parameter, this contains all data */
-			type:         null,                   /* Specify type of lightbox. If unset, it will check for the targetAttrs value. */
-			contentFilters: ['jquery', 'image', 'html', 'ajax', 'text'], /* List of content filters to use to determine the content */
+		constructor: Featherlight,
+		/*** defaults ***/
+		/* extend featherlight with defaults and methods */
+		namespace:    'featherlight',         /* Name of the events and css class prefix */
+		targetAttr:   'data-featherlight',    /* Attribute of the triggered element that contains the selector to the lightbox content */
+		variant:      null,                   /* Class that will be added to change look of the lightbox */
+		resetCss:     false,                  /* Reset all css */
+		background:   null,                   /* Custom DOM for the background, wrapper and the closebutton */
+		openTrigger:  'click',                /* Event that triggers the lightbox */
+		closeTrigger: 'click',                /* Event that triggers the closing of the lightbox */
+		filter:       null,                   /* Selector to filter events. Think $(...).on('click', filter, eventHandler) */
+		root:         'body',                 /* Where to append featherlights */
+		openSpeed:    250,                    /* Duration of opening animation */
+		closeSpeed:   250,                    /* Duration of closing animation */
+		closeOnClick: 'background',           /* Close lightbox on click ('background', 'anywhere' or false) */
+		closeOnEsc:   true,                   /* Close lightbox when pressing esc */
+		closeIcon:    '&#10005;',             /* Close icon */
+		otherClose:   null,                   /* Selector for alternate close buttons (e.g. "a.close") */
+		beforeOpen:   $.noop,                 /* Called before open. can return false to prevent opening of lightbox. Gets event as parameter, this contains all data */
+		beforeClose:  $.noop,                 /* Called before close. can return false to prevent opening of lightbox. Gets event as parameter, this contains all data */
+		afterOpen:    $.noop,                 /* Called after open. Gets event as parameter, this contains all data */
+		afterClose:   $.noop,                 /* Called after close. Gets event as parameter, this contains all data */
+		type:         null,                   /* Specify type of lightbox. If unset, it will check for the targetAttrs value. */
+		contentFilters: ['jquery', 'image', 'html', 'ajax', 'text'], /* List of content filters to use to determine the content */
 
-			/*** methods ***/
-			/* setup iterates over a single instance of featherlight and prepares the background and binds the events */
-			setup: function(target, config){
-				/* all arguments are optional */
-				if (typeof target === 'object' && target instanceof $ === false && !config) {
-					config = target;
-					target = undefined;
+		/*** methods ***/
+		/* setup iterates over a single instance of featherlight and prepares the background and binds the events */
+		setup: function(target, config){
+			/* all arguments are optional */
+			if (typeof target === 'object' && target instanceof $ === false && !config) {
+				config = target;
+				target = undefined;
+			}
+
+			var self = $.extend(this, config, {target: target}),
+				css = !self.resetCss ? self.namespace : self.namespace+'-reset', /* by adding -reset to the classname, we reset all the default css */
+				$background = $(self.background || '<div class="'+css+'"><div class="'+css+'-content"><span class="'+css+'-close-icon '+ self.namespace + '-close">'+self.closeIcon+'</span></div></div>'),
+				closeButtonSelector = '.'+self.namespace+'-close' + (self.otherClose ? ',' + self.otherClose : '');
+
+			self.$instance = $background.clone().addClass(self.variant); /* clone DOM for the background, wrapper and the close button */
+
+			/* attach esc handler to document if configured */
+			if(self.closeOnEsc && escapeHelper) {
+				$(document).bind('keyup.'+this.constructor.prototype.namespace, escapeHelper);
+				escapeHelper = null;
+			}
+
+			/* close when click on background/anywhere/null or closebox */
+			self.$instance.on(self.closeTrigger+'.'+self.namespace, function(event) {
+				var $target = $(event.target);
+				if( ('background' === self.closeOnClick  && $target.is('.'+self.namespace))
+					|| 'anywhere' === self.closeOnClick
+					|| $target.is(closeButtonSelector) ){
+					event.preventDefault();
+					self.close();
 				}
+			});
 
-				var self = $.extend(this, config, {target: target}),
-					css = !self.resetCss ? self.namespace : self.namespace+'-reset', /* by adding -reset to the classname, we reset all the default css */
-					$background = $(self.background || '<div class="'+css+'"><div class="'+css+'-content"><span class="'+css+'-close-icon '+ self.namespace + '-close">'+self.closeIcon+'</span></div></div>'),
-					closeButtonSelector = '.'+self.namespace+'-close' + (self.otherClose ? ',' + self.otherClose : '');
+			return this;
+		},
 
-				self.$instance = $background.clone().addClass(self.variant); /* clone DOM for the background, wrapper and the close button */
+		/* this method prepares the content and converts it into a jQuery object or a promise */
+		getContent: function(){
+			var self = this,
+				filters = this.constructor.contentFilters,
+				readTargetAttr = function(name){ return self.$currentTarget && self.$currentTarget.attr(name); },
+				targetValue = readTargetAttr(self.targetAttr),
+				data = self.target || targetValue || '';
 
-				/* attach esc handler to document if configured */
-				if(self.closeOnEsc && escapeHelper) {
-					$(document).bind('keyup.'+this.constructor.prototype.namespace, escapeHelper);
-					escapeHelper = null;
-				}
+			/* Find which filter applies */
+			var filter = filters[self.type]; /* check explicit type like {type: 'image'} */
 
-				/* close when click on background/anywhere/null or closebox */
-				self.$instance.on(self.closeTrigger+'.'+self.namespace, function(event) {
-					var $target = $(event.target);
-					if( ('background' === self.closeOnClick  && $target.is('.'+self.namespace))
-						|| 'anywhere' === self.closeOnClick
-						|| $target.is(closeButtonSelector) ){
-						event.preventDefault();
-						self.close();
+			/* check explicit type like data-featherlight="image" */
+			if(!filter && data in filters) {
+				filter = filters[data];
+				data = self.target && targetValue;
+			}
+			data = data || readTargetAttr('href') || '';
+
+			/* check explicity type & content like {image: 'photo.jpg'} */
+			if(!filter) {
+				for(var filterName in filters) {
+					if(self[filterName]) {
+						filter = filters[filterName];
+						data = self[filterName];
 					}
+				}
+			}
+
+			/* otherwise it's implicit, run checks */
+			if(!filter) {
+				var target = data;
+				data = null;
+				$.each(self.contentFilters, function() {
+					filter = filters[this];
+					if(filter.test)  {
+						data = filter.test(target);
+					}
+					if(!data && filter.regex && target.match && target.match(filter.regex)) {
+						data = target;
+					}
+					return !data;
 				});
-
-				return this;
-			},
-
-			/* this method prepares the content and converts it into a jQuery object or a promise */
-			getContent: function(){
-				var self = this,
-					filters = this.constructor.contentFilters,
-					readTargetAttr = function(name){ return self.$currentTarget && self.$currentTarget.attr(name); },
-					targetValue = readTargetAttr(self.targetAttr),
-					data = self.target || targetValue || '';
-
-				/* Find which filter applies */
-				var filter = filters[self.type]; /* check explicit type like {type: 'image'} */
-
-				/* check explicit type like data-featherlight="image" */
-				if(!filter && data in filters) {
-					filter = filters[data];
-					data = self.target && targetValue;
-				}
-				data = data || readTargetAttr('href') || '';
-
-				/* check explicity type & content like {image: 'photo.jpg'} */
-				if(!filter) {
-					for(var filterName in filters) {
-						if(self[filterName]) {
-							filter = filters[filterName];
-							data = self[filterName];
-						}
-					}
-				}
-
-				/* otherwise it's implicit, run checks */
-				if(!filter) {
-					var target = data;
-					data = null;
-					$.each(self.contentFilters, function() {
-						filter = filters[this];
-						if(filter.test)  {
-							data = filter.test(target);
-						}
-						if(!data && filter.regex && target.match && target.match(filter.regex)) {
-							data = target;
-						}
-						return !data;
-					});
-					if(!data) {
-						if('console' in window){ window.console.error('Featherlight: no content filter found ' + (target ? ' for "' + target + '"' : ' (no target specified)')); }
-						return false;
-					}
-				}
-				/* Process it */
-				return filter.process.call(self, data);
-			},
-
-			/* sets the content of $instance to $content */
-			setContent: function($content){
-				var self = this;
-				/* we need a special class for the iframe */
-				if($content.is('iframe') || $('iframe', $content).length > 0){
-					self.$instance.addClass(self.namespace+'-iframe');
-				}
-				self.$content = $content.addClass(self.namespace+'-inner');
-
-				/* remove existing content */
-				self.$instance.find('.'+self.namespace+'-inner').remove();
-				self.$instance.find('.'+self.namespace+'-content').append(self.$content);
-
-				return self;
-			},
-
-			/* opens the lightbox. "this" contains $instance with the lightbox, and with the config */
-			open: function(event){
-				var self = this;
-				if(event && !event.isDefaultPrevented()
-					|| self.beforeOpen(event) !== false) {
-
-					if(event){
-						event.preventDefault();
-					}
-					var $content = self.getContent();
-
-					if($content){
-						/* Add to opened registry */
-						self.constructor._opened.add(self._openedCallback = function(response){
-							if(self.$instance.closest('body').length > 0) {
-								response.currentFeatherlight = self;
-							}
-						});
-
-						/* Set content and show */
-						$.when($content).done(function($content){
-							self.setContent($content)
-								.$instance.appendTo(self.root).fadeIn(self.openSpeed);
-							self.afterOpen(event);
-						});
-						return self;
-					}
-				}
-				return false;
-			},
-
-			/* closes the lightbox. "this" contains $instance with the lightbox, and with the config */
-			close: function(event){
-				var self = this;
-				if(self.beforeClose(event) === false) {
+				if(!data) {
+					if('console' in window){ window.console.error('Featherlight: no content filter found ' + (target ? ' for "' + target + '"' : ' (no target specified)')); }
 					return false;
 				}
-				self.constructor._opened.remove(self._openedCallback);
-				self.$instance.fadeOut(self.closeSpeed,function(){
-					self.$instance.detach();
-					self.afterClose(event);
-				});
 			}
+			/* Process it */
+			return filter.process.call(self, data);
+		},
+
+		/* sets the content of $instance to $content */
+		setContent: function($content){
+			var self = this;
+			/* we need a special class for the iframe */
+			if($content.is('iframe') || $('iframe', $content).length > 0){
+				self.$instance.addClass(self.namespace+'-iframe');
+			}
+			self.$content = $content.addClass(self.namespace+'-inner');
+
+			/* remove existing content */
+			self.$instance.find('.'+self.namespace+'-inner').remove();
+			self.$instance.find('.'+self.namespace+'-content').append(self.$content);
+
+			return self;
+		},
+
+		/* opens the lightbox. "this" contains $instance with the lightbox, and with the config */
+		open: function(event){
+			var self = this;
+			if(event && !event.isDefaultPrevented()
+				|| self.beforeOpen(event) !== false) {
+
+				if(event){
+					event.preventDefault();
+				}
+				var $content = self.getContent();
+
+				if($content){
+					/* Add to opened registry */
+					self.constructor._opened.add(self._openedCallback = function(response){
+						if(self.$instance.closest('body').length > 0) {
+							response.currentFeatherlight = self;
+						}
+					});
+
+					/* Set content and show */
+					$.when($content).done(function($content){
+						self.setContent($content)
+							.$instance.appendTo(self.root).fadeIn(self.openSpeed);
+						self.afterOpen(event);
+					});
+					return self;
+				}
+			}
+			return false;
+		},
+
+		/* closes the lightbox. "this" contains $instance with the lightbox, and with the config */
+		close: function(event){
+			var self = this;
+			if(self.beforeClose(event) === false) {
+				return false;
+			}
+			self.constructor._opened.remove(self._openedCallback);
+			self.$instance.fadeOut(self.closeSpeed,function(){
+				self.$instance.detach();
+				self.afterClose(event);
+			});
+		}
 	};
 
 	$.extend(Featherlight, {
