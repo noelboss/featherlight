177a8c87ed23a322b1f71f38131b620157b0564c
Marc-Andre Lafortune <github@marc-andre.ca>
2014-11-17 10:17:20 -0500

    Release Version 1.0.1

--- src/featherlight.js
+++ src/featherlight.js
@@ -1,6 +1,6 @@
 /**
  * Featherlight - ultra slim jQuery lightbox
- * Version 1.0.0 - http://noelboss.github.io/featherlight/
+ * Version 1.0.1 - http://noelboss.github.io/featherlight/
  *
  * Copyright 2014, Noël Raoul Bossart (http://www.noelboss.com)
  * MIT Licensed.
