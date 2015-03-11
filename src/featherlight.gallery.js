08e1bfcff61c0f7452fa94bf7114778fba6aecb8
Marc-Andre Lafortune <github@marc-andre.ca>
2015-03-11 14:12:25 -0400

    Release Version 1.2.2

--- src/featherlight.gallery.js
+++ src/featherlight.gallery.js
@@ -1,6 +1,6 @@
 /**
  * Featherlight Gallery – an extension for the ultra slim jQuery lightbox
- * Version 1.2.1 - http://noelboss.github.io/featherlight/
+ * Version 1.2.2 - http://noelboss.github.io/featherlight/
  *
  * Copyright 2015, Noël Raoul Bossart (http://www.noelboss.com)
  * MIT Licensed.
