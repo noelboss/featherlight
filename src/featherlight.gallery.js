0f08dcb428c52262d9f37c8e3af5c59c6e8a6b32
Sean Kennedy <sean@splitleaf.net>
2015-06-11 15:03:43 -0400

    Adding afterNavigateTo method

--- src/featherlight.gallery.js
+++ src/featherlight.gallery.js
@@ -98,6 +98,7 @@
 		nextIcon: '&#9654;',         /* Code that is used as next icon */
 		galleryFadeIn: 100,          /* fadeIn speed when image is loaded */
 		galleryFadeOut: 300,         /* fadeOut speed before image is loaded */
+        afterNavigateTo: $.noop,     /* Called after navigate to loads the slide, gets index as parameter */
 
 		slides: function() {
 			if (this.filter) {
@@ -131,7 +132,9 @@
 					self.setContent($newContent);
 					self.afterContent();
 					$newContent.fadeTo(self.galleryFadeIn,1);
-			});
+			})
+            /* Call afterNavigateTo after fadeIn is done */
+            .done(function(){ self.afterNavigateTo(index); });
 		},
 
 		createNavigation: function(target) {
