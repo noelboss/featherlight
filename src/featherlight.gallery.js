a0cf29019eedd4f4acde10b3b563725605e95e57
Jude Gibbons <judes@gimaju.net>
2019-04-22 15:42:37 +0100

    Change navigation spans into anchor tags.
    Links are more semantically-correct than spans.
    Anchor tags have native tabindex.
    Edit css to work with anchor tags rather than spans.

--- src/featherlight.gallery.js
+++ src/featherlight.gallery.js
@@ -148,7 +148,7 @@
 
 		createNavigation: function(target) {
 			var self = this;
-			return $('<span title="'+target+'" class="'+this.namespace+'-'+target+'"><span>'+this[target+'Icon']+'</span></span>').click(function(evt){
+			return $('<span title="'+target+'" class="'+this.namespace+'-'+target+'"><a href="#">'+this[target+'Icon']+'</a></span>').click(function(evt){
 				$(this).trigger(target+'.'+self.namespace);
 				evt.preventDefault();
 			});
